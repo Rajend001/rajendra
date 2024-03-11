e52cd9ce87a8af7305f9447789f32d18b049b2b5
Rajend001 <162985961+Rajend001@users.noreply.github.com>
2024-03-11 16:29:32 +0530

    Add files via upload

---
+++ file_ py1..py
@@ -0,0 +1,79 @@
+Day one morning
+print('hello world')
+1.)commenting is used for making documentation is a python file
+2.)when the is commected , that line will not considerd as a part of code.
+3.)when the program is executed commented line will not be in the line of execution.
+---->note:
+    single connecting can be done using # at the begining of the line
+       Eg:
+           #print("hello 1")
+multiple line commenting can be done using '''code'''.the line between the quotes
+eg:
+print("hello")
+"""
+print("hello")
+'''
+inport this
+(second afteroon) 1:00 to 5;00
+
+Bargained characters
+controls and data structures --->ABC language
+strings and its formats --->from C
+regular expressions --->pearl
+exception handling --->module3
+magic methods from --->fortron
+
+What kind of language  is python
+python is a
+---> object oriented programing language
+--->dynamically typed language
+--->scripting language
+--->interpreated language
+--->compiled language
+--->functional programing language
+
+Flavours of python
+-->Cpython-written in c,most common implementation pf python
+-->jython-java implementation of python
+-->ironpython-implementation of the python programing language which is tightly intregated with ,NET,
+-->brython-browser python ,runs in browsers
+-->rubypython-bridge between python and ruby interpreters
+-->micropython-runs in micro controllers
+
+print
+#-->print(object,sep,end,file,flush)
+#printds has 5 parameters
+
+#object --->the value to be printed is object
+#sep-->seperator
+#Eg
+#a=8
+#b=9
+#c=89
+#print(a,b,c,sep'$')
+#end -->which is used to add the dtring at the end of the value,
+# # it will not keep the cammand to say in same line
+#Eg:
+#print(str(a),b end ='%%')
+print(str(C))
+
+#!?--->)variables
+?variable is a name which is used to identify the value of variable
+#?which is stored in a specific memory location
+#! rules to define variables
+1,variable have to start with only alphabets ,underscores
+2,the char folling the 1st char can be alphabets ,numbers ,underscores
+3,no spaces are allowed
+4,no special charasters like !@#$%^&()+
+5,Case sentitive
+6.keywods are not allowed as variables
+
+Ways to define variables
+#a=8
+#b=9
+#print(a,b)
+
+#a=7,b=9 # error
+#print(a,b)
+
+
