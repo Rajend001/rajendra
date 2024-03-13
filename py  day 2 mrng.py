11946c72d34eebd8cf43eb9163a625702b1d694a
Rajend001 <162985961+Rajend001@users.noreply.github.com>
2024-03-13 09:15:25 +0530

    Add files via upload

---
+++ py  day 2 mrng.py
@@ -0,0 +1,168 @@
+12-03-2024
+day two morning
+
+ variables
+ eg:
+a,b=c=7,8
+print(a)
+print(b)
+print(C)
+ans:
+7
+8
+(7,8)
+--->swapping of variables
+a=7
+b=5
+print(a,b)
+temp=a temp=7
+a=b b=5
+b=temp b=7
+
+a=5,b=7
+print(a,b)
+
+eg:2
+a=a+b a=12
+b=a-b b=12-5=5
+a=a-b a=12-5=7
+pribt(a,b)
+a,b=b,a only in python
+print(a,b)
+
+a=a*b a=35
+b+a/b b=35/7 =5.0
+a=a/b b=35/5 =7.0
+prin(int(a),int(b))
+id() --> used to find the memory address of the variable
+a=7
+b=8
+print(id(a))
+print(id(b))
+
+#-->keywords
+keywords are reserved words which provides special meaning to complier or interpretor
+import keyword
+print(keyword.kwlist)
+print(len(keyword .kwlist))
+print(type(keyword .kwlist))
+
+
+To check if the string is keyword or not
+print(keyword .iskeyword("sid")) false
+
+if=8
+print(if) error coz if is a keyword
+
+#---->literals
+literals is the constant value assigned to a variable
+A variable is consider to be a constant when it is defines
+in caps
+
+a=78 a is variable
+A=78 A is constant
+
+
+hash ()--> it creates a hash value for constant datatypes and
+provides erroer for non constant datatypes
+n1=89+7j
+print(hash(n1))
+
+f1=[7,8,9]
+print(hash(f1))error -->list is non-constant or mutable datatypes
+
+a=9
+b=9
+b=90
+print(id(a))
+print(id(b))
+!--->operators
+operators are symbols which is used to perform various operations
+between 2 0r more operators
+Arithmatic
+assignment
+logical
+relational or comparsion
+bitwise
+identity
+memership
+
+---> Arithmatic --->+,-,*,%,//,**
+a=8
+b=6
+c=9
+print(a+b+C)
+
+
+input()-->used to get the runtime  input
+eva1()--> used to get the runtime values of all datatypes
+
+n1=eva1(input ("Enter the value:"))
+print(n1)
+
+a=4
+b=2
+print(a/b)is used to get the quoient value
+print(a%b)is used to get the remainder value
+print(a/b) is used to get the quotient value print(a%b) is used to get the remainder value #!//-->floor devision a=765433 b=19 print(a//b)--> the output will be in integer & the output will be based on floor value #!**--> used for power of a number print(2**4)--->16 #!Assignment -->=+=,-=,/=,*=,//=,**=,&=,|= a=8 a+=2 print(a) a=30 a-=5 print(a) #! comparison--> ==,>,<,!=,>= a=8 b=7 print(a>b) True a=9 b=5 print(a==b) #! Bitwise operator -->&,|,^,~,<<,>> a=7 b=4 print(a&b) 2^4 2^3 2^2 2^1 2^0 8 4 2 1 0 1 1 1 --->7 0 1 0 0 --->4& ---------------- 0 1 0 0 a=7 b=6 print(a|b) AND A B A&B 0 0 0 0 1 0 1 0 0 1 1 1 OR A B A|B 0 0 0 0 1 1 1 0 1 1 1 1 #! Logical--> used to compare the expressions and , or , not a=6 print(a>3 and a<10) print(a>7 or a<30) print(not(a>8 and a<10)) #! identity operator is, is not a=8 b=8 print(a is not b) #! membership operator in, not in 11=[7,8,9,0,6,5] num=55 prin...
+Inbox
+
+
+
+n=int(input("enter the number:"))
+if n=%2==0
+print(n,"is even")
+else:
+    print(n,"odd")
+name,age,nationality
+18,above,indian
+name =input ("enter the name" )
+age=int("enter the age")
+nationality=input("enter the nation:"
+if age>18 and nationality=="india"
+print("eligble foe vote")
+else:
+    print("not eligble")
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
