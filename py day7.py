00d07545ecd03a7387a0cbe242e48c5cd9ad4ad4
Rajend001 <162985961+Rajend001@users.noreply.github.com>
2024-03-18 11:53:16 +0530

    Add files via upload

---
+++ py day7.py
@@ -0,0 +1,115 @@
+18/03/24 day 7 morning
+
+d1={'Ten':10,'Twenty':20,'Thirty':30}
+d2={'Thirty':30,'fourty':40,'fifty':50}
+d1.update(d2)
+
+set1 = {'python','java','Data science'}
+set2 = {'ML','AI','R Language','Python'}
+set3 = {'Data Analytics','Robotics','Deep Learning'}
+c = 0
+for val in range(3):
+    c=c+1
+    if c==1:
+       for val in set1:
+           if val in set2 or val in set3:
+            flag=1
+            break
+           
+    if c==2:
+       for val in set2:
+           if val in set1 or val in set3:
+              flag=1
+              break
+             
+    if c==3:
+       for val in set3:
+           if val in set2 or val in set1:
+              flag=1
+              break
+
+
+3.
+list1 = ['m',"na","i","ke"]
+list2 = ["y","me","s","lly"]
+
+result = [list1[i] + list2[i]
+for i in range(len(list1))]
+
+print(result)
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+i=0
+while i<len(list1):
+    l3.append(list1[i]+list2[i])
+    i+=1
+print(l3)
+
+ hacker rank-->caesar cipher
+
+! functions
+DEF
+function is a block of code which is used to perform a specific functionality
+functions can be created using def keyword
+
+#generally pythoan has a 3 parts
+
+function decleration
+function defanition
+function call
+
+! eg:
+def greet(): function defination
+    print("welcome user!!")
+
+greet()
+greet()    
+greet()  
+greet()  
+greet()  
+greet()  
+greet()function calling
+
+eg;2
+# function with parameter
+
+def greeting(name):
+    print("welcome",name)
+
+for val in range(3):
+    username = input("Enter the name:")
+    greeting(username) username is argument
+
+eg:3
+11.) Write a Python script to generate and print a dictionary that 
+# contains a number (between 1 and n) in the form (x, x*x).
+# Sample Dictionary ( n = 5) :
+# Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+# 2.)Find the uncommon words from 2 strings
+
+#s1 = "Hello how are you"
+#s2 = "Hello how is"
+
+# 3.)Wrire a code print the 8th fibanochi number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
