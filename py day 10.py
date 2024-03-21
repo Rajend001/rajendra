7b69d01a9d08a554845743bd6c03fa00ef7f47d2
Rajend001 <162985961+Rajend001@users.noreply.github.com>
2024-03-21 15:51:48 +0530

    Add files via upload

--- py day 10.py
+++ py day 10.py
@@ -122,6 +122,137 @@ obj.add(1,2,3)
 
 
 #----->abstraction
+The process of hiding the implimentation details is abstraction
+
+Eg:1
+
+from abc import ABC,abstractmethod
+class shape(ABC):
+    @abstractmethod
+class shape(self):
+    def sides(self):
+        print("All shapes have sides expect circles")
+
+class triangle:
+    def triangle_sides(self):
+        print("3sides")
+
+    def name(self):
+        print("iam triangle")
+
+    def sides(self):
+        pass
+
+class square(shapes):
+    def square(self):
+        print("4 sides")
+
+    def square(self):
+        pass
+        
+tr = triangle()
+tr.triangle_sides()
+tr.name()
+
+#rules to define abstact class1
+1.Absract class cannot be instatained
+2.from abc import ABC,abstractmethod
+3.subclass the normal class with ABC
+4.covert the normal method inside the abstact class to abstact method
+5.All the child classes have to be subclasses with abstact class
+6.the abstract method should be present in the child classes
+
+
+Eg:2
+super()--> used to access the parent class method from child method
+from abc  import ABC,abstractmethod
+class c1(ABC):
+    @abstractmethod
+    def m1(self):
+        print("This is abstract class")
+
+
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print("Iam child 1")
+
+    def m1(self)
+        pass
+
+class2 = c2()
+class2.m2()
+
+
+eg:3
+from abc import ABC,abstractmethod
+class password:
+    @abstractmethod
+    def pwd(self):
+        pswd = "sidd1994$"
+        return pswd
+
+class login:
+    def validate(self,name,parent):
+        if super().pwd()== password:
+           print("welcome",name,'!!')
+           print("login,successfull")
+     else:
+           print("please check the password")
+
+    def pwd(self):
+        pass
+
+Login = login()
+name = input("Enter the name: ")
+pwd = input("Enter the password: ")
+Login.validate(name,pwd)
+
+
+#Encapsulation
+eg:1
+class car:
+   _name = "BMW" private variable
+   print(_name)
+
+c1 = car()
+print(c1.name)# error
+c1.name = "Audi"
+print(c1.name) # error
+
+Eg:2
+#! accessing private data outside the class
+class c1:
+    _phone = '7676767676'
+
+    def display(self):
+        print(self._phone)
+c = c1()
+c.display()
+
+
+--->Eg:3
+declare private method
+class class1:
+    def _m1(self): #private method
+        print("Iam private method")
+
+    def_init_(self):
+        self._m1()
+c = class1()
+c._m1() #error
+
+?nested class:
+class class1:
+    class class2:
+        name = "sidhu"
+
+        def display(self):
+            print(self.name)
+    obj1 = class2()
+
+obj = class1()
+obj.obj1.display()
 
 
 
@@ -134,17 +265,6 @@ obj.add(1,2,3)
 
 
 
-
-
-
-
-
-
-
-a = 9
-b = 6
-print(a+b)
-print(a._add_(b))# dunder method or mafic method
 
 
 
